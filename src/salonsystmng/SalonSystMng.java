a673bc2c3cfc63df45651a5454b97c48a793e871
mzoun <realmkq@gmail.com>
2023-01-23 21:53:58 +0300

    mzoun commit

---
+++ src/salonsystmng/SalonSystMng.java
@@ -0,0 +1,15 @@
+
+package salonsystmng;
+//mzoun khaled
+//hadeel
+//ragad
+public class SalonSystMng {
+
+
+    public static void main(String[] args) {
+       
+        
+        
+    }
+    
+}
