ee28c3b17eb75b485c6014985f7713ba3c844a70
mzoun <realmkq@gmail.com>
2023-01-23 22:32:09 +0300

    mzoun commit

--- src/salonsystmng/SalonSystMng.java
+++ src/salonsystmng/SalonSystMng.java
@@ -7,6 +7,8 @@ public class SalonSystMng {
 
 
     public static void main(String[] args) {
+       user cust1=new customer("mzoun", "alqarni","2006750", "mzoun750", "mkq@gmail.com","0501571845");
+       user cust2=new customer("rahaf", "khaled","1906766", "rhf766", "rhf@gmail.com","0501456783");
        
         
         
